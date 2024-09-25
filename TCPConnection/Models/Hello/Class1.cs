2b8ab832b86fd7ecf2a04d83516c682ed5e2083e
Anton <sharlai.anton@booksoft.com.ua>
2024-09-25 17:57:01 +0300

    Added new folder Hello

---
+++ TCPConnection/Models/Hello/Class1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class MyCollection : IEnumerable<string>
+{
+    private List<string> items = new List<string>();
+
+    public void Add(string item)
+    {
+        items.Add(item);
+    }
+
+    public IEnumerator<string> GetEnumerator()
+    {
+        foreach (string item in items)
+        {
+            yield return item;
+        }
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return this.GetEnumerator();
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine(1.1 * 100);
+    }
+}
